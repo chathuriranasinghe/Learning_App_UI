49c00c00098080806bbba619dc803a4230bcc626
chathuriranasinghe <chathusudarshiransinghe@gmail.com>
2024-08-21 22:14:52 +0530

    Finalized home page

---
+++ lib/models/class_model.dart
@@ -0,0 +1,8 @@
+class ClassModel {
+  String name;
+  String image;
+  int studentCount;
+
+  ClassModel(
+      {required this.name, required this.image, required this.studentCount});
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app_ui/models/class_model.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<ClassModel> classes = [
+    ClassModel(
+        name: "Mathematics", image: "assets/maths.png", studentCount: 200),
+    ClassModel(name: "Music", image: "assets/music.jpeg", studentCount: 150),
+    ClassModel(
+        name: "English", image: "assets/english.jpeg", studentCount: 375),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.note_rounded), label: "My Works"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded), label: "Account")
+      ]),
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
@@ -20,6 +40,20 @@ class HomePage extends StatelessWidget {
           color: Colors.purple,
         ),
       ),
+      body: ListView.builder(
+          itemCount: classes.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(classes[index].image),
+              ),
+              title: Text(
+                classes[index].name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Student Count - ${classes[index].studentCount}"),
+            );
+          }),
     );
   }
 }
